e00406245d26f1f4b39e415865ba46ed726d1b4d
Will Bidstrup <willie@williemcrae.com>
2020-09-03 12:05:31 +0930

    feat: tested sound, failed

---
+++ game_dev/sounds.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Import playsound module
+from playsound import playsound
+
+# Input an existing mp3 filename
+# Play the mp3 file
+playsound('/Users/williambidstrup/Documents/GitHub/yabp/game_dev/Sounds/beat.mp3')
